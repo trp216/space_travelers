46626747a67fb326fa3285d209b2130b2034f708
Alejandra Diaz <alejandradp19@gmail.com>
2020-11-18 16:38:20 -0500

    Changed the class to an interface

--- src/collections/Graph.java
+++ src/collections/Graph.java
@@ -6,58 +6,40 @@
 
 package collections;
 
-public final class Graph<B> {
+public interface Graph<B> {
+		
+	//------------------------------------------------------------------------------------
+	
+	public boolean addVertex(Graph<B> graph, B vertex) ;
 	
 	//------------------------------------------------------------------------------------
 	
-	public Graph(boolean wightedGraph, boolean directedGraph) {}
+	public boolean addEdge(Graph<B> graph, B v1, B v2, int weight) ;
 	
 	//------------------------------------------------------------------------------------
 	
-	public boolean addVertex(Graph<B> graph, B vertex) {
-		
-	}
-	
-	//------------------------------------------------------------------------------------
-	
-	public boolean addEdge(Graph<B> graph, B v1, B v2, int weight) {
-		
-	}
-	
-	//------------------------------------------------------------------------------------
-	
-	public boolean addEdge(Graph<B> graph, B v1, B v2) {
-		
-	}
+	public boolean addEdge(Graph<B> graph, B v1, B v2) ;
 	
 	//------------------------------------------------------------------------------------
 		
-	public boolean isEmpty(Graph<B> graph) {
-		
-	}
+	public boolean isEmpty(Graph<B> graph) ;
 	
 	//------------------------------------------------------------------------------------
 	
-	public boolean removeVertex(Graph<B> graph, B vertex) {
-		
-	}
+	public boolean removeVertex(Graph<B> graph, B vertex) ;
 	
 	//------------------------------------------------------------------------------------
 	
-	public boolean removeEdge(Graph<B> graph, B v1, B v2) {
-		
-	}
+	public boolean removeEdge(Graph<B> graph, B v1, B v2) ;
 	
 	//------------------------------------------------------------------------------------
 	
-	public B search(Graph<B> graph) {
-		
-	}
+	public B search(Graph<B> graph) ;
 	
 	//------------------------------------------------------------------------------------
 	
-	public int[][] getWeightedMatrix(){
-		
-	}
+	public int[][] getWeightedMatrix() ;
+	
+	//------------------------------------------------------------------------------------
 
 }
