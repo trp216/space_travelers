e10465a659d13dd24347b0be704f66a8cccc6558
Alejandra Diaz <alejandradp19@gmail.com>
2020-11-18 16:32:41 -0500

    Added skeletons methods in Graph.

--- src/collections/Graph.java
+++ src/collections/Graph.java
@@ -1,5 +1,63 @@
+/*
+ * ALGORITMOS Y ESTRUCTURAS DE DATOS
+ * TAREA INTEGRADORA 2
+ * MARTINEZ - DIAZ - RODAS
+ */
+
 package collections;
 
-public class Graph {
+public final class Graph<B> {
+	
+	//------------------------------------------------------------------------------------
+	
+	public Graph(boolean wightedGraph, boolean directedGraph) {}
+	
+	//------------------------------------------------------------------------------------
+	
+	public boolean addVertex(Graph<B> graph, B vertex) {
+		
+	}
+	
+	//------------------------------------------------------------------------------------
+	
+	public boolean addEdge(Graph<B> graph, B v1, B v2, int weight) {
+		
+	}
+	
+	//------------------------------------------------------------------------------------
+	
+	public boolean addEdge(Graph<B> graph, B v1, B v2) {
+		
+	}
+	
+	//------------------------------------------------------------------------------------
+		
+	public boolean isEmpty(Graph<B> graph) {
+		
+	}
+	
+	//------------------------------------------------------------------------------------
+	
+	public boolean removeVertex(Graph<B> graph, B vertex) {
+		
+	}
+	
+	//------------------------------------------------------------------------------------
+	
+	public boolean removeEdge(Graph<B> graph, B v1, B v2) {
+		
+	}
+	
+	//------------------------------------------------------------------------------------
+	
+	public B search(Graph<B> graph) {
+		
+	}
+	
+	//------------------------------------------------------------------------------------
+	
+	public int[][] getWeightedMatrix(){
+		
+	}
 
 }
