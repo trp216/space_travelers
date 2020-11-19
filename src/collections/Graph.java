e3e624a584f8b028cef9a368145c9efe4180a5ba
Santiago <santiago.rodas1@correo.icesi.edu.co>
2020-11-18 21:05:48 -0500

    Some coments in the class Graph

--- src/collections/Graph.java
+++ src/collections/Graph.java
@@ -15,49 +15,73 @@ public interface Graph<V> {
 		
 	//------------------------------------------------------------------------------------
 	
+	// METHOD 1 (INTERFACE)
+	
 	public boolean addVertex(V vertex) ;
 	
 	//------------------------------------------------------------------------------------
+	
+	// METHOD 2 (INTERFACE)
 	
 	public boolean addEdge(V v1, V v2, int weight) ;
 	
 	//------------------------------------------------------------------------------------
 	
+	// METHOD 3 (INTERFACE)
+	
 	public boolean addEdge(V v1, V v2) ;
 	
 	//------------------------------------------------------------------------------------
+	
+	// METHOD 4 (INTERFACE)
 		
 	public boolean isEmpty() ;
 	
 	//------------------------------------------------------------------------------------
 	
+	// METHOD 5 (INTERFACE)
+	
 	public boolean removeVertex(V vertex) ;
 	
 	//------------------------------------------------------------------------------------
 	
+	// METHOD 6 (INTERFACE)
+	
 	public boolean removeEdge(V v1, V v2) ;
 	
 	//------------------------------------------------------------------------------------
+	
+	// METHOD 7 (INTERFACE)
 
 	public Hashtable<V,Hashtable<V,Integer>> getWeightMatrix() ;
 
 	//------------------------------------------------------------------------------------
+	
+	// METHOD 8 (INTERFACE)
 
 	public Hashtable<V,List<Pair<V,Integer>>> getAdjacencyList() ;
 	
 	//------------------------------------------------------------------------------------
 	
+	// METHOD 9 (INTERFACE)
+	
 	public List<V> getEdgeList() ;
 
 	//------------------------------------------------------------------------------------
+	
+	// METHOD 10 (INTERFACE)
 	
 	public List<V> getEdgeList(V vertex) ;
 
 	//------------------------------------------------------------------------------------
 	
+	// METHOD 11 (INTERFACE)
+	
 	public Hashtable<V,V> getVertexList() ;
 
 	//------------------------------------------------------------------------------------
+	
+	// METHOD 12 (INTERFACE)
 	
 	public List<V> getAdjacentVertices(V vertex) ;
 
