b79b2e87f613a743b8cd37638bb2a91df36050d8
Santiago <santiago.rodas1@correo.icesi.edu.co>
2020-11-30 10:07:52 -0500

    :(

---
+++ src/ui/Main.java
@@ -0,0 +1,37 @@
+/*
+ * ALGORITMOS Y ESTRUCTURAS DE DATOS
+ * TAREA INTEGRADORA 3
+ * DIAZ - MARTINEZ - RODAS
+ */
+
+package ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	//------------------------------------------------------------------------------------
+	
+	// METODO START DEL METODO MAIN
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		
+		
+	}
+	
+	//------------------------------------------------------------------------------------
+	
+	// METODO MAIN PRINCIPAL (EJECUTABLE)
+	
+	public static void main(String[] args) {
+		
+		launch(args);
+		
+	}
+	
+	//------------------------------------------------------------------------------------
+
+}
