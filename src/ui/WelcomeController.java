f66e35444ebcc3854cf5948a039322a0d17ca5cd
Alejandra Diaz <alejandradp19@gmail.com>
2020-12-04 10:15:48 -0500

    Implemented method to select the kind of graph to use in the program in WelcomeController.

--- src/ui/WelcomeController.java
+++ src/ui/WelcomeController.java
@@ -2,42 +2,66 @@ package ui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import model.NavigationSystem;
 
 public class WelcomeController {
-	
+
 	private NavigationSystem navSys;
-	
+
 	public WelcomeController(NavigationSystem navigationSystem) {
 		navSys = navigationSystem;
 	}
 
-    @FXML
-    private AnchorPane welcomeAP;
+	@FXML
+	private AnchorPane welcomeAP;
 
-    @FXML
-    private RadioButton tipo1;
+	@FXML
+	private RadioButton tipo1;
 
-    @FXML
-    private ToggleGroup tipoGrafo;
+	@FXML
+	private ToggleGroup tipoGrafo;
 
-    @FXML
-    private RadioButton tipo2;
+	@FXML
+	private RadioButton tipo2;
 
-    @FXML
-    private Button tipoGrafoButton;
+	@FXML
+	private Button tipoGrafoButton;
 
-    @FXML
-    void selectGrafo(ActionEvent event) {
+	@FXML
+	void selectGrafo(ActionEvent event) {
 
-    }
-    
-    @FXML
-    void initializa() {
-    	
-    }
+		if(tipo1.isSelected()) {
+			navSys.graphSelected(true);
+		}
+		else if(tipo2.isSelected()) {
+			navSys.graphSelected(false);
+		}
+		else {
+			insufficientDataAlert();
+		}
+
+	}
+	
+	@FXML
+	void insufficientDataAlert() {
+
+		Alert alert = new Alert(AlertType.ERROR);
+		alert.setTitle("Sorry");
+		alert.setHeaderText("Some necessary fields are empty");
+		alert.setContentText("Please fill the missing fields to continue with the operation");
+
+		alert.showAndWait();
+
+	}
+
+	@FXML
+	void initializa() {
+
+	}
 }
